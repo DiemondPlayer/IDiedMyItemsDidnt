e0c00ead64df6420a3587732670e9f6c39618f15
Diemond_Player <diemondplay@gmail.com>
2024-10-10 09:50:32 +0500

    tag support

--- src/main/java/net/diemond_player/idmid/mixin/PlayerInventoryMixin.java
+++ src/main/java/net/diemond_player/idmid/mixin/PlayerInventoryMixin.java
@@ -1,6 +1,7 @@
 package net.diemond_player.idmid.mixin;
 
 import net.diemond_player.idmid.util.IDMIDAccessor;
+import net.diemond_player.idmid.util.IDMIDTags;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,7 +12,9 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public abstract class PlayerInventoryMixin {
 	@ModifyArg(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;dropItem(Lnet/minecraft/item/ItemStack;ZZ)Lnet/minecraft/entity/ItemEntity;"), method = "dropAll")
 	private ItemStack init(ItemStack stack) {
-		((IDMIDAccessor)((Object)stack)).idmid$setAsDeathDrop(true);
+		if(!(stack.isIn(IDMIDTags.Items.FORCE_DESPAWN))){
+			((IDMIDAccessor)((Object)stack)).idmid$setAsDeathDrop(true);
+		}
 		return stack;
 	}
 }
---
+++ src/main/java/net/diemond_player/idmid/util/IDMIDTags.java
@@ -0,0 +1,17 @@
+package net.diemond_player.idmid.util;
+
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+public class IDMIDTags {
+    public static class Items {
+        public static final TagKey<Item> FORCE_DESPAWN =
+                createItemTag("force_despawn");
+
+        private static TagKey<Item> createItemTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, Identifier.of("idmid", name));
+        }
+    }
+}
